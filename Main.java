1bf04d86d4e06aa61732c21c90ee376ec61a032f
ElPelucaDLAP <agusnarsaniga@proton.me>
2025-04-01 18:36:38 -0300

    2.0
    Mejoras

--- Main.java
+++ Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-         //Desafío: Desarrolla unaaplicación para gestionar tareas pendientes. Crea una clase Tarea
+        //Desafío: Desarrolla unaaplicación para gestionar tareas pendientes. Crea una clase Tarea
         // con atributos como descripción y estado (completada o no). Luego, permite al usuario agregar
         // nuevas tareas y marcarlas como completadas.
         Scanner sc = new Scanner(System.in);
         ArrayList<Tarea> tareasDisponibles = new ArrayList<>();
         System.out.println("Ingrese una nueva tarea: ");
-        tareasDisponibles.add(new Tarea("Ordenar Archivos","Los archivos de la biblioteca" +
+        tareasDisponibles.add(new Tarea("Ordenar Archivos", "Los archivos de la biblioteca" +
                 " estan desorganizados", 1));
-        tareasDisponibles.add(new Tarea("Organizar Cuentas","Las cuentas del negocio se" +
+        tareasDisponibles.add(new Tarea("Organizar Cuentas", "Las cuentas del negocio se" +
                 "encuentran desordenadas", 1));
         System.out.println("Las tareas actuales son: ");
         for (int i = 0; i < tareasDisponibles.size(); i++) {
@@ -24,20 +24,20 @@ public class Main {
         String descripcion = sc.nextLine();
         System.out.println("Estado: ");
         int estado = sc.nextInt();
-        System.out.println("modificar un estado, elija la tarea a modificar:");
-        int numeroDeTareaAmodificar = sc.nextInt(); sc.nextLine();
         tareasDisponibles.add(new Tarea(nombre, descripcion, estado));
-        System.out.println("Cambia estado de una tarea");
+        System.out.println("Cambia estado de las tareas");
         System.out.println("1.Sin hacer");
         System.out.println("2.En Proceso");
         System.out.println("3.Completa");
-        int estadoTarea= sc.nextInt(); sc.nextLine();
-        tareasDisponibles.get(numeroDeTareaAmodificar).setEstado(estadoTarea);
-        System.out.println(tareasDisponibles.get(numeroDeTareaAmodificar).toString());
+        for (int i = 0; i < tareasDisponibles.size(); i++) {
+            int estadoTarea = sc.nextInt();
+            sc.nextLine();
+            tareasDisponibles.get(i).setEstado(estadoTarea);
+            System.out.println(tareasDisponibles.get(i).toString());
 
-
-
-
-
+        }
+        for (int i = 0; i < tareasDisponibles.size(); i++) {
+            System.out.println(tareasDisponibles.get(i).toString());
+        }
     }
 }
